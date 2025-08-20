43e3eaf5f7cb5e2920854732a0617b25b48c16fb
Shubhamyadav@65381 <shubham65381@gmail.com>
2025-08-20 16:42:40 +0530

    homepage is ready

--- src/components/social/social.jsx
+++ src/components/social/social.jsx
@@ -12,16 +12,36 @@ const Social = () => {
           rel="noopener noreferrer"
           className="group relative w-14 h-14 rounded-full border-2 border-white bg-transparent hover:bg-white cursor-pointer transition-colors duration-300 flex items-center justify-center"
         >
-          <img
-            src={images.facebook_wh}
-            alt="Facebook"
-            className="absolute inset-0 w-7 h-7 mx-auto my-auto transition-opacity duration-300 group-hover:opacity-0"
-          />
-          <img
-            src={images.facebook_bl}
-            alt="Facebook"
-            className="absolute inset-0 w-7 h-7 mx-auto my-auto opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-          />
+          <div
+            className="relative w-7 h-7"
+            style={{
+              transformStyle: 'preserve-3d',
+              transition: 'transform 0.5s',
+            }}
+          >
+            <img
+              src={images.facebook_wh}
+              alt="Facebook"
+              className="absolute inset-0 w-7 h-7"
+              style={{
+                backfaceVisibility: 'hidden',
+              }}
+            />
+            <img
+              src={images.facebook_bl}
+              alt="Facebook"
+              className="absolute inset-0 w-7 h-7"
+              style={{
+                backfaceVisibility: 'hidden',
+                transform: 'rotateX(180deg)',
+              }}
+            />
+          </div>
+          <style jsx>{`
+            a:hover > div {
+              transform: rotateX(180deg);
+            }
+          `}</style>
         </a>
 
         {/* Instagram */}
@@ -32,16 +52,36 @@ const Social = () => {
           className="group relative w-14 h-14 flex items-center justify-center rounded-full border-2 border-white bg-transparent hover:bg-white cursor-pointer transition-colors duration-300"
           style={{ boxSizing: 'border-box' }}
         >
-          <img
-            src={images.inst_wh}
-            alt="Instagram"
-            className="absolute inset-0 w-7 h-7 mx-auto my-auto transition-opacity duration-300 group-hover:opacity-0"
-          />
-          <img
-            src={images.insta_bl}
-            alt="Instagram"
-            className="absolute inset-0 w-7 h-7 mx-auto my-auto opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-          />
+          <div
+            className="relative w-7 h-7"
+            style={{
+              transformStyle: 'preserve-3d',
+              transition: 'transform 0.5s',
+            }}
+          >
+            <img
+              src={images.inst_wh}
+              alt="Instagram"
+              className="absolute inset-0 w-7 h-7"
+              style={{
+                backfaceVisibility: 'hidden',
+              }}
+            />
+            <img
+              src={images.insta_bl}
+              alt="Instagram"
+              className="absolute inset-0 w-7 h-7"
+              style={{
+                backfaceVisibility: 'hidden',
+                transform: 'rotateX(180deg)',
+              }}
+            />
+          </div>
+          <style jsx>{`
+            a:hover > div {
+              transform: rotateX(180deg);
+            }
+          `}</style>
         </a>
 
         {/* X (Twitter) */}
@@ -49,19 +89,39 @@ const Social = () => {
           href="#"
           target="_blank"
           rel="noopener noreferrer"
-          className="group relative w-14 h-14 flex items-center justify-center rounded-full border-2 hover:bg-white border-white bg-transparent transition-colors duration-300"
+          className="group relative w-14 h-14 flex items-center justify-center rounded-full border-2 border-white bg-transparent hover:bg-white cursor-pointer transition-colors duration-300"
           style={{ boxSizing: 'border-box' }}
         >
-          <img
-            src={images.x_white}
-            alt="X"
-            className="absolute inset-0 w-7 h-7 mx-auto my-auto transition-opacity duration-300 group-hover:opacity-0"
-          />
-          <img
-            src={images.x_black}
-            alt="X"
-            className="absolute inset-0 w-7 h-7 mx-auto my-auto opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-          />
+          <div
+            className="relative w-7 h-7"
+            style={{
+              transformStyle: 'preserve-3d',
+              transition: 'transform 0.5s',
+            }}
+          >
+            <img
+              src={images.x_white}
+              alt="X"
+              className="absolute inset-0 w-7 h-7"
+              style={{
+                backfaceVisibility: 'hidden',
+              }}
+            />
+            <img
+              src={images.x_black}
+              alt="X"
+              className="absolute inset-0 w-7 h-7"
+              style={{
+                backfaceVisibility: 'hidden',
+                transform: 'rotateX(180deg)',
+              }}
+            />
+          </div>
+          <style jsx>{`
+            a:hover > div {
+              transform: rotateX(180deg);
+            }
+          `}</style>
         </a>
       </div>
     </div>
